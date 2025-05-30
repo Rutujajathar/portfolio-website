b11c0a7b8b64bafbd0594f232f67fcd4dfbbf80a
Rutujajathar <rutujajathar.rj@gmail.com>
2025-05-30 13:08:11 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -1,1 +1,29 @@
 
+class Pattern
+{
+	public static void main(String[]args)
+	{
+	int a=1;
+	for(int i=1;i<=5;i++)
+	{
+	int b=a+i-1;
+	for(int j=1;j<=5;j++)
+	{
+		if(i>=j)
+		{
+		if(i%2==1)
+	
+			System.out.print("\t"+ a );
+		
+		else
+		
+			System.out.print("\t"+  b );
+			a++;
+			b--;
+		}
+	}
+	System.out.println( );
+	}
+	}
+
+}
