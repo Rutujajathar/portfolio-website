709bed9bb20fb498ac6f335dca495569b2859db9
Rutujajathar <rutujajathar.rj@gmail.com>
2025-05-30 13:48:16 +0530

    Update constractionoverloadding.java

--- constractionoverloadding.java
+++ constractionoverloadding.java
@@ -1,1 +1,17 @@
 
+class ConstructorOverloading 
+{
+	ConstructorOverloading()
+	{
+		System.out.println("no argument constructor");
+	}
+	ConstructorOverloading(int a)
+	{
+		System.out.println("intger argument");
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+
+	}
+}
