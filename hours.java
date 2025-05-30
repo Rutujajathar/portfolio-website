3555af711a2f340801637153ffb1f14399bca669
Rutujajathar <rutujajathar.rj@gmail.com>
2025-05-30 13:07:21 +0530

    Update hours.java

--- hours.java
+++ hours.java
@@ -1,1 +1,13 @@
 
+import java.util.Scanner;
+class hours
+{
+	public static void main(String[] args)
+	{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter minute");
+	int minute=sc.nextInt();
+	int a= minute / 60;
+	System.out.println("hours= " + a);	
+	}
+}
