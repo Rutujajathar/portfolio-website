cfcc4852c1aa4298f5e723b4295eb2227b676e95
Rutujajathar <rutujajathar.rj@gmail.com>
2025-05-30 13:46:43 +0530

    Update methodoverloadding.java

--- methodoverloadding.java
+++ methodoverloadding.java
@@ -1,1 +1,25 @@
 
+class MethodOverloading 
+{
+	public static void main(String[] args)
+	{
+		add(2,3);
+		add(2,3,4);
+		add(2,3,4,5);
+	}
+
+	public static int add(int a, int b)
+	{
+		return a+b;
+	}
+	public static int add(int a, int b, int c)
+	{
+		return a+b+c;
+	}
+
+	public static int add(int a, int b, int c, int d )
+	{
+		return a+b+c+d;
+	}
+}
+
