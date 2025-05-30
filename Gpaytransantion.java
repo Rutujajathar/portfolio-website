1dcf5f7f1539456e1576c8347180f738bdc8e98e
Rutujajathar <rutujajathar.rj@gmail.com>
2025-05-30 13:44:56 +0530

    Update Gpaytransantion.java

--- Gpaytransantion.java
+++ Gpaytransantion.java
@@ -1,1 +1,26 @@
 
+import java.util.Scanner;
+class Gpay1
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int cp=123;
+		System.out.println("Enter the contact number");
+		long a=sc.nextLong();
+		System.out.println("enter the money");
+		double money=sc.nextDouble();
+		System.out.println("Enter the password");
+		int cpc=sc.nextInt();
+		if(cpc==cp)
+		{
+		System.out.println("transantion successful");
+		System.out.print( a +"\t"+money );
+		
+		}
+		else
+		{
+			System.out.println("Invalid password");
+		}
+	}
+}
